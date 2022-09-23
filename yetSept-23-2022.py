181f297758c49ce313a39e0045cde1d305ed41ef
yetblac <yetsondblack@gmail.com>
2022-09-23 16:56:13 -0400

    Septemeber 23 2022, C200 Lab3

---
+++ yetSept-23-2022.py
@@ -0,0 +1,4 @@
+testString = "This is a String to Show Operations"
+print(testString[1:6:2])
+    # in this example [1:6] is used to show "This I" 
+    # then :2] skips every other character creating "hsi"
